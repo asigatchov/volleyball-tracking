0b12e005391825fafbcbf79113628024366f68dd
Alexander Sigatchov <sa@inventos.ru>
2025-05-13 09:27:28 +0300

    rgb ball

--- run_train_1024_gray3frame.py
+++ run_train_1024_gray3frame.py
@@ -44,28 +44,3 @@ model.train(
     #    resume=True,
 )
 
-# model.train(
-#     data='/home/nssd/gled/vb/dataset-vb/sideline/quick/crop/data.yaml',
-#     imgsz=640,
-#     epochs=200,  # Увеличено для мелких объектов
-#     batch=24,    # Максимально возможный размер
-#     lr0=1e-4,
-#     augment=True,
-#     hsv_h=0.3,   # Увеличенный диапазон
-#     hsv_s=0.5,   # Увеличенная насыщенность
-#     degrees=10,  # Добавлен поворот
-#     flipud=0.5,
-#     mixup=0.2,
-#     translate=0.2,  # Новый параметр
-#     mosaic=1.0,     # Добавлен мозаик
-#     cache=True,
-#     optimizer='AdamW',
-#     pretrained=True,
-#     weight_decay=0.05,
-#     cos_lr=True,    # Косинусный шедулер
-#     lrf=0.1,
-#     rect=True,      # Прямоугольное обучение
-#     close_mosaic=10 # Отключение мозаика в конце
-# )
-
-#
