dfbd193147a5d6c991eccb0aa34fbc382b2293d0
Alexander Sigatchov <asigatchov@gmail.com>
2025-06-03 17:58:27 +0300

    badminton

--- run_train_1024_gray3frame.py
+++ run_train_1024_gray3frame.py
@@ -4,10 +4,10 @@ from ultralytics import YOLO
 #
 last_model = "models/defaults/yolo11n.pt"
 last_model = "models/asigatchov/yolo11n_gray3frame_ball_1024_e100_5k.pt"
-last_model = 'runs/detect/train14/weights/last.pt'
+#last_model = 'runs/detect/train18/weights/last.pt'
 model = YOLO(last_model)
 
-data = "/home/projects/www/vb-soft/datasets/transmash_with_action/crop/data.yaml"
+data = "/home/nssd/gled/vb/dataset/badminton/crop/data.yaml"
 
 # data = '/home/projects/www/vb-soft/datasets/hokkey/crop/data.yaml'
 # В data.yaml
@@ -23,7 +23,7 @@ model.train(
     data=data,
     imgsz=1024,
     epochs=50,
-    batch=12,
+    batch=12,   
     lr0=1e-4,
     augment=True,  # false не сработало?
     scale=0.25,
@@ -41,7 +41,6 @@ model.train(
     pretrained=True,
     rect=True,  # Прямоугольное обучение
     mosaic=0.0,  # Отключить мозаику, если кадры последовательны
-    # resume=True,
+#    resume=True,
     #
 )
-
